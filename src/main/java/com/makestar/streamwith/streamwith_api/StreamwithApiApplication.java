43f000e1df261c3d48ddfb9dd614caf9ecaa6e91
chanhos-alchera <ch.jung@alcherainc.com>
2024-04-29 14:41:28 +0900

    Initial commit

---
+++ src/main/java/com/makestar/streamwith/streamwith_api/StreamwithApiApplication.java
@@ -0,0 +1,13 @@
+package com.makestar.streamwith.streamwith_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StreamwithApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StreamwithApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/makestar/streamwith/streamwith_api/StreamwithApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.makestar.streamwith.streamwith_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StreamwithApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
